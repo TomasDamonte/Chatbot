7e3a02cece4486bf8ee3498539f394ab9c47b9c6
ivan.jinkus <ivan.jinkus@CPX-LOVO79XQ3W7.dir.svc.accenture.com>
2017-11-23 19:04:00 -0300

    Clase de mapa con respuestas. Cree la clase de manejo de Excepciones

---
+++ Chatbot/src/ChatBotExceptions.java
@@ -0,0 +1,8 @@
+
+public class ChatBotExceptions extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
---
+++ Chatbot/src/mapaRespuestas.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+
+public class mapaRespuestas {
+	
+	public mapaRespuestas() {
+		this.getMapaRespuestas();
+	}
+	
+	private HashMap<Integer, String> mapaRespuestas = new HashMap<Integer, String>();
+
+	private HashMap<Integer, String> getMapaRespuestas() {
+		return mapaRespuestas;
+	}
+
+	private void setMapaRespuestas(HashMap<Integer, String> mapaRespuestas) {
+		this.mapaRespuestas = mapaRespuestas;
+	}
+	
+	private String getRespuesta(Integer key) {
+		if (this.getMapaRespuestas().containsKey(key)) {
+			return this.getMapaRespuestas().get(key);
+		}
+		return null;
+	}
+	
+	private void addRespuesta(String respuesta) {
+		Integer key = this.getMapaRespuestas().size() + 1;
+		this.getMapaRespuestas().put(key, respuesta);
+	}
+}

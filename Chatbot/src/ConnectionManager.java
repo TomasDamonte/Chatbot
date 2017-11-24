0bee528c3858c6355fd26d4bd4090d9a6eb6a525
tomas <tomas@damonte>
2017-11-24 18:02:13 -0300

    +Query para buscar por autor

--- Chatbot/src/ConnectionManager.java
+++ Chatbot/src/ConnectionManager.java
@@ -59,4 +59,28 @@ public class ConnectionManager {
 		}	
 		return libros;
 	}
+	
+	public static LinkedList<Libro> getLibrosAutor(String str) { 
+		LinkedList<Libro> libros = new LinkedList<>();
+		try {
+			Connection conn = DriverManager.getConnection(url, username, password);
+			Statement stmt = (Statement) conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from biblioteca where autor like '%"+str+"%'");
+			while (rs.next()) {
+				String titulo = rs.getString(2);	
+				String autor = rs.getString(3);
+				String editorial = rs.getString(4);
+				String genero = rs.getString(5);
+				String fechaPublicacion = rs.getString(6);
+				Libro libro = new Libro (titulo,autor,editorial,genero,fechaPublicacion);
+				libros.add(libro);
+			}
+			stmt.close();
+			conn.close();
+			rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		return libros;
+	}
 }
--- Chatbot/src/TesteoDB.java
+++ Chatbot/src/TesteoDB.java
@@ -4,7 +4,7 @@ public class TesteoDB {
 
 	public static void main(String[] args) {		
 		
-		for(Libro libro : ConnectionManager.getLibrosTitulo("pist")) {
+		for(Libro libro : ConnectionManager.getLibrosAutor("kin")) {
 			System.out.println(libro.toString());
 		}
 		

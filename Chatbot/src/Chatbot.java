4eeae4bf86de3e0abce1073053a66acc3603ca50
tomas <tomas@damonte>
2018-02-06 10:58:27 -0300

    merged MainParaTestear con Chatbot

--- Chatbot/src/Chatbot.java
+++ Chatbot/src/Chatbot.java
@@ -1,7 +1,49 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-class Chatbot {
-	
-	public static void main(String[] args) {
-		System.out.println("hola soy main");
+public class Chatbot {
+
+	public static void main(String[] args) throws Exception {
+
+		Boolean nuevaConsulta;
+		InputProvider inputProvider = new InputProvider();
+		Conversacion.mensajeInicial();
+		Conversacion.preguntaNombrePersona();
+		Persona persona = new Persona(inputProvider.getStringInput());
+		do {
+			System.out.println(persona.getNombre());
+			Conversacion.preguntaTipoConsulta();
+			AnalizadorConsulta analizadorConsulta = new AnalizadorConsulta(inputProvider.getStringInput());
+			while(!analizadorConsulta.analizarConsulta()) {
+				Conversacion.preguntaTipoConsulta();
+				analizadorConsulta = new AnalizadorConsulta(inputProvider.getStringInput());
+			}
+			LinkedList<Libro> libros = new LinkedList<Libro>();
+			if(analizadorConsulta.getBusqueda().equals("titulo")) {
+				Conversacion.preguntaEspecificarConsulta("libro");
+				libros = ConnectionManager.getLibrosTitulo(inputProvider.getStringInput());
+			}
+			else if(analizadorConsulta.getBusqueda().equals("autor")){
+				Conversacion.preguntaEspecificarConsulta("autor");
+				libros = ConnectionManager.getLibrosAutor(inputProvider.getStringInput());
+			}
+			else {
+				ArrayList<String> categorias = ConnectionManager.getCategorias();
+				Conversacion.categoriasDisponibles();		
+				for(String categoria : categorias) 
+					System.out.println(categoria);
+				Conversacion.preguntaCategoria();
+				libros = ConnectionManager.getLibrosCategoria(inputProvider.getStringInput());
+			}
+			if(!libros.isEmpty()) {				
+				for(Libro libro : libros) 
+					Conversacion.libroEncontrado(libro);				
+			}
+			else Conversacion.consultaNoEncontrada();
+			Conversacion.preguntaNuevaConsulta();
+			if(inputProvider.getStringInput().equalsIgnoreCase("si")) nuevaConsulta = true;
+			else nuevaConsulta = false;
+		}while(nuevaConsulta);
+		Conversacion.mensajeFinal();
 	}
 }
